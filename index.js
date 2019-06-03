0849d7921f03189c1861327b5c8fd903394853b6
JordanG <jmg236@yahoo.com>
2019-06-03 18:01:58 -0500

    delete n put

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ server.get("/api/users", (req, res) => {
     })
     .catch(err => {
       res.status(500).json({
-        error: "The users information could not be retrieved.",
+        error: "This users information could not be retrieved.",
         message: err
       });
     });
@@ -29,10 +29,49 @@ server.get("/api/users/:id", (req, res) => {
     .catch(err => {
       res.status(404).json({
         error: err,
-        message: "does not exist."
+        message: "doesn't exist."
       });
     });
 });
+
+server.delete("/api/users/:id", (req, res) => {
+    const UserId = req.params.id;
+    db.remove(UserId)
+      .then(user => {
+        if (user) {
+          db.remove(UserId).then(removeruser => {
+            res.status(201).json(removeruser);
+          });
+        } else {
+          res
+            .status(404)
+            .json({
+              error: err,
+              mesage: "This user with specified ID does no exist"
+            });
+        }
+      })
+      .catch(error => {
+        res.status(500).json({ message: "This user could not be removed" });
+      });
+  });
+  server.put("/api/users/:id", (req, res) => {
+    const userId = req.params.id;
+    const userBody = req.body;
+    db.update(userId, userBody)
+      .then(user => {
+        if (user) {
+          db.findById(userId).then(userupdate => {
+            res.status(201).json(userupdate);
+          });
+        } else {
+          res.status(400).json({ error: err, message: "Can't be found" });
+        }
+      })
+      .catch(error => {
+        res.status(404).json({ message: "Can't be found" });
+      });
+  });
 
 server.listen(5000, () => {
   console.log(`Server running on localhost:5000`);
